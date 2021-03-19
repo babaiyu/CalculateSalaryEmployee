c254370e682c13a02ee3efc11888ac3842c9d561
babaiyu <bayuputra9a@gmail.com>
2021-03-19 22:56:45 +0700

    Changed

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("fast-csv");
 
-const UPAH_PER_JAM = 30000;
+// Konstanta
+const UPAH_PER_JAM = 3000;
 
+// Main Function
 function main() {
   let arr = [];
+  let arrPrice = [];
 
+  // Read file from karyawan.csv
   fs.createReadStream(path.resolve(__dirname, "assets", "karyawan.csv"))
     .pipe(csv.parse({ headers: true }))
     .on("error", (error) => console.error(error))
@@ -14,13 +18,17 @@ function main() {
       arr.push(row);
     })
     .on("end", (totalRow) => {
+      // Calculate gaji
       let rate = UPAH_PER_JAM,
         total = 0;
       for (let i = 0; i < totalRow; i++) {
         let totalReguler = rate * arr[i].jam_reguler;
         let totalLembur = rate * arr[i].jam_lembur * 1.5;
-        total += totalReguler + totalLembur;
+        let totalResult = totalReguler + totalLembur;
+        arrPrice.push(totalResult);
+        total += totalResult;
       }
+      console.table(arrPrice);
       console.log(`Gaji anda = Rp. ${total}`);
     });
 }
